bee907f33575ae8856753322df898cc83d916688
Emre KARAMAN <matizdrum@gmail.com>
2022-11-13 15:33:48 +0300

    basic component structure complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 import './App.css';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Header />
       <div className='container'>
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
